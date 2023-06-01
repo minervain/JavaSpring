2cdd38e3059e6163141cddc97080934804ecfbd4
minervain <aliosmanucar987@gmail.com>
2023-06-01 14:42:05 +0300

    Update

--- NotHesaplama/src/Main.java
+++ NotHesaplama/src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int Mat, Fiz, Kim, tr;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("MAT:");
+         Mat =input.nextInt();
+        System.out.println("Fiz:");
+        Fiz =input.nextInt();
+        System.out.println("Kim:");
+        Kim =input.nextInt();
+        System.out.println("tr:");
+        tr =input.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int Ort=(Mat+Fiz+Kim+tr)/4;
+         String Gectimi= Ort>50 ? "Geçti" : "kaldı";
+
+        System.out.println(Gectimi);
     }
 }
